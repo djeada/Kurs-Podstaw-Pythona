36a473a3712762c7735594b02e21ebfee4aae3b8
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2019-07-08 22:56:38 +0100

    Add files via upload

---
+++ Trojkaty.py
@@ -0,0 +1,40 @@
+"""\
+*
+**
+***
+"""
+for i in range(0, 3):
+    for j in range(2-i, 3):
+        print('*',end='')
+    print('\r')
+
+print('')
+print('')
+
+"""\
+1
+11
+111
+"""
+for i in range(0, 3):
+    for j in range(2-i, 3):
+        print('1',end='')
+    print('\r')
+
+print('')
+print('')
+
+"""\
+1
+12
+123
+"""
+for i in range(0, 3):
+    for j in range(0,i+1):
+        print(j+1,end='')
+    print('\r')
+
+print('')
+print('')
+
+
