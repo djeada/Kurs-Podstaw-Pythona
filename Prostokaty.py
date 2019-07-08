88751e1203f92c8e43c06b577d5c4d3aca8ebf6d
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2019-07-08 22:56:47 +0100

    Add files via upload

---
+++ Prostokaty.py
@@ -0,0 +1,50 @@
+"""\
+***
+***
+***
+"""
+for i in range(0, 3):
+    for j in range(0, 3):
+        print('*',end='')
+    print('\r')
+
+print('')
+print('')
+
+"""\
+1111
+1111
+1111
+"""
+for i in range(0, 3):
+    for j in range(0, 4):
+        print('1',end='')
+    print('\r')
+
+print('')
+print('')
+
+"""\
+1234
+1234
+1234
+"""
+for i in range(0, 3):
+    for j in range(0, 4):
+        print(j+1,end='')
+    print('\r')
+
+print('')
+print('')
+
+"""\
+123
+456
+789
+"""
+for i in range(0, 3):
+    for j in range(0, 3):
+        print(j+1+3*i,end='')
+    print('\r')
+
+
