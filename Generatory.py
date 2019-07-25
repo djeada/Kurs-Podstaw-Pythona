046f07e3dd758dada5c95ceb965915370b6074de
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2019-07-25 12:01:53 +0100

    Add files via upload

--- Generatory.py
+++ Generatory.py
@@ -1,17 +1,42 @@
+#generator to funkcja to tworzenia iteratora
+
 def generator():
     yield 1
     yield 2
-    yield 3
-
+    yield 8
+    
+print('Generator1: ')
 
 for i in generator():
     print(i)
 
+x = generator()
+print(next(x))
+print(next(x))
+print(next(x))
 
 def generator2(a,b):
-    while a < b:
+    while a <= b:
         yield a
-        a+=1
+        a +=3
 
-for i in generator2(4,10):
+print('')
+print('Generator2: ')
+
+for i in generator2(2,20):
     print(i)
+
+print('')
+print('Odwroc: ')
+
+def odwroc(dane):
+    for i in range(len(dane)-1,-1,-1):
+        yield dane[i]
+
+for i in odwroc('kukulka'):
+    print(i,end='')
+
+for i in odwroc((4,3,9,'k',False)):
+    print(i,end='')
+        
+    
