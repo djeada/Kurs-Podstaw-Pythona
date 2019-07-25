c05aba1562bf6baa18727733fdfe46fe4984689f
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2019-07-25 11:28:28 +0100

    Add files via upload

--- Iteratory.py
+++ Iteratory.py
@@ -1,27 +1,26 @@
-#iterator to obiekt ktory sluzy do przegladania elementow struktury
+#iterator to obiekt ktory przegladania elementow kolekcji
 #listy, zbiory, slowniki, krotki sa iterowalne
 
-lista = [2, 9, 3]
+lista = [1, 3, 4]
 
-moj_iterator = iter(lista)
-print(moj_iterator)
-print(next(moj_iterator))
-print(next(moj_iterator))
-print(next(moj_iterator))
+iterator_listowy = iter(lista)
+print(iterator_listowy)
+print(next(iterator_listowy))
+print(next(iterator_listowy))
+print(next(iterator_listowy))
+#print(next(iterator_listowy))
 
 #iter inicjuje iteracje
-#next zwraca wartosc danego elementu, przechodzi do nastepnego
-#i zwraca blad gdy doszlismy do konca
-
+#next zwraca wartosc danego elementu i przechodzi do nastepnego
+#zwraca wyjatek gdy dojdzie do konca
 
 class MojIterator():
-    def __init__(self):
+    def __init__(self, maks=10):
         self.x = 1
-        self.max = 10
+        self.max = maks
 
     def __iter__(self):
         return self
-
 
     def __next__(self):
         x = self.x
@@ -29,15 +28,42 @@ class MojIterator():
         if x > self.max:
             raise StopIteration
 
-        self.x += 1
+        self.x +=5
         return x
 
-for i in MojIterator():
+
+for i in MojIterator(40):
     print(i)
 
 
+class Odwroc():
+    def __init__(self, dane='Napis'):
+        self.dane = dane
+        self.indeks = len(dane)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.indeks == 0:
+            raise StopIteration
+        
+        self.indeks -= 1
+        return self.dane[self.indeks]
+
+for i in Odwroc():
+    print(i, end='')
+
+print('')
+
+for i in Odwroc('tunczyk'):
+    print(i, end='')
+   
+print('')
+
+for i in Odwroc((3,2,'e',10)):
+    print(i, end='')
 
 
-
-
-
+    
+        
