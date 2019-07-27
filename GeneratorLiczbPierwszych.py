02385a693bf5bf033eb1baa9a914345975f73df0
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2019-07-27 22:21:49 +0100

    Add files via upload

--- GeneratorLiczbPierwszych.py
+++ GeneratorLiczbPierwszych.py
@@ -11,4 +11,26 @@ def gen_primes(max_number):
         if isprime(num1):
             yield num1
 
-print(list(gen_primes(3311111)))
+print(list(gen_primes(333)))
+
+
+class itPierwszych():
+    def __init__(self, n):
+        self.i = 0
+        self.n = n
+        
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        while True:
+            self.i+=1
+            if self.i > self.n:
+                raise StopIteration
+            if isprime(self.i):
+                return self.i
+
+print(list(itPierwszych(333)))
+
+
+
