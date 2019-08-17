965d832bc83217637d4fa22f1ef68649cb8ec0eb
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2019-08-17 17:42:22 +0100

    Add files via upload

---
+++ Autogui.py
@@ -0,0 +1,11 @@
+import pyautogui
+
+pyautogui.displayMousePosition()
+doubleClick()
+moveTo(x,y,seconds)
+moveRel(x,y,seconds)
+dragTo(x,y,seconds)
+pyautogui.click(-1800,100)
+pyautogui.typewrite('Hello')
+pyautogui.typewrite(['enter'])
+hotkey('ctr','c')
