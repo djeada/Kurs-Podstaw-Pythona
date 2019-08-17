1a162824c991c9e01753923b3884c6d4bdd554f2
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2019-08-17 15:31:06 +0100

    Add files via upload

--- Wyrazenia_Regularne2.py
+++ Wyrazenia_Regularne2.py
@@ -3,23 +3,33 @@ import re
 #+48-999-999-999
 
 #\d to jest dowolna cyfra
-#\D to jest dowolny znak nie bedacy cyfra
+#\D to jest dowolny znak nie badacy cyfra
 
-wzorNumeru = re.compile(r'[+]\d{2,3}-\d{3}-\d{3}-\d{3}')
-
-znaleziono = wzorNumeru.search('To jest moj numer telefonu +48-999-999-999')
-znaleziono2 = wzorNumeru.search('To jest moj numer telefonu +32-919-943-009')
-znaleziono3 = wzorNumeru.search('To jest moj numer telefonu +352-939-559-999')
+wzorNumeru = re.compile(r'[+]\d{1,4}-\d{3}-\d{3}-\d{3}')
+znaleziono = wzorNumeru.search('Moj numer to +48-999-999-999')
+znaleziono2 = wzorNumeru.search('Moj numer to +352-919-459-933')
+znaleziono3 = wzorNumeru.search('Moj numer to +7-943-865-000')
 
 print(znaleziono.group())
 print(znaleziono2.group())
 print(znaleziono3.group())
 
-numerWzor = re.compile(r'([+]\d\d)-(\d\d\d-\d\d\d-\d\d\d)')
-znajdz = numerWzor.search(sentence)
-print(znajdz.group(1))
-print(znajdz.group(2))
+wzorNumeru = re.compile(r'([+]\d{1,4})-(\d{3}-\d{3}-\d{3})')
+znaleziono = wzorNumeru.search('Moj numer to +48-999-999-999')
+print(znaleziono.group(1))
+print(znaleziono.group(2))
 
 
-#moze nie musi byc 
-numerWzor = re.compile(r'([+]\d\d)?-\d\d\d-\d\d\d-\d\d\d')
+wzorNumeru = re.compile(r'([+]\d{1,4}-)?\d{3}-\d{3}-\d{3}')
+znaleziono = wzorNumeru.search('Moj numer to 888-999-999')
+znaleziono2 = wzorNumeru.search('Moj numer to +99-999-999-999')
+
+print(znaleziono.group())
+print(znaleziono2.group())
+
+
+
+
+
+
+
