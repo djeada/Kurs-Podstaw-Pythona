a90e16b7ee2063f3aa8005ebbad7bfe35f3038ae
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2019-08-17 16:48:18 +0100

    Add files via upload

---
+++ GrafikaZolwia.py
@@ -0,0 +1,24 @@
+import turtle
+
+def kwadrat(nazwa, dlugosc):
+    for i in range(4):
+        nazwa.pensize(i*5)
+        nazwa.forward(dlugosc)
+        nazwa.left(90)
+
+def skomplikowany(nazwa):
+    for i in range(300):
+        nazwa.forward(i)
+        nazwa.right(88)
+
+zolwik = turtle.Turtle()
+zolwik.speed(1)
+zolwik.pencolor('blue')
+#rysujemy kwadrat
+kwadrat(zolwik, 100)
+kwadrat(zolwik, 200)
+
+#rysujemy inny ksztalt
+skomplikowany(zolwik)
+
+turtle.exitonclick()
