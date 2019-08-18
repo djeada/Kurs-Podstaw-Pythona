afc23a2c2bd29fddcfc282272564ede9bafa78f7
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2019-08-18 10:15:34 +0100

    Add files via upload

--- Autogui.py
+++ Autogui.py
@@ -1,22 +1,23 @@
 import pyautogui
 import time
+#pyautogui.displayMousePosition()
+
+'''
+pyautogui.click(-110, 48)
+pyautogui.click(-1344, 1326)
+'''
+
 pyautogui.typewrite(['win'])
 pyautogui.typewrite('paint')
+time.sleep(1)
 pyautogui.typewrite(['enter'])
 time.sleep(1)
-pyautogui.doubleClick(-1100,550)
-pyautogui.dragRel(300,0, 2)
-pyautogui.dragRel(0,300, 2)
+pyautogui.click(-1100, 500)
+pyautogui.dragRel(300,0,2)
+pyautogui.dragRel(0,300,2)
 
-pyautogui.displayMousePosition()
-doubleClick()
-moveTo(x,y,seconds)
-moveRel(x,y,seconds)
-dragTo(x,y,seconds)
-pyautogui.click(-1800,100)
-pyautogui.typewrite('Hello')
-pyautogui.typewrite(['enter'])
-hotkey('ctr','c')
+
+
 
 
 
