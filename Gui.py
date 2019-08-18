78b7bd43e2e5e12346f8a5ec237d53264ea7720e
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2019-08-18 12:16:05 +0100

    Add files via upload

--- Gui.py
+++ Gui.py
@@ -3,22 +3,17 @@ import tkinter
 root = tkinter.Tk()
 root.geometry('480x480')
 
-l = tkinter.Label(root, text='bibo')
+l = tkinter.Label(root, text='Aplikacja')
 l.pack()
 
-def buttonFunction():
-    print('Hello world!')
+def funkcjaPrzycisku():
+    print('Wcisnieto przycisk')
 
-b = tkinter.Button(root, text='Duplo', command=buttonFunction)
-b.place(x=0,y=0)
+b = tkinter.Button(root, text='Jestem Przyciskiem1', width=10, bg='red', fg='white', command=funkcjaPrzycisku)
+b.pack(side=tkinter.RIGHT)
+b2 = tkinter.Button(root, text='Jestem Przyciskiem2', command=funkcjaPrzycisku)
+b2.pack(side=tkinter.BOTTOM)
+b3 = tkinter.Button(root, text='Jestem Przyciskiem3', command=funkcjaPrzycisku)
+b3.place(x=380,y=0)
 
 root.mainloop()
-
-
-
-
-
-
-
-
-
