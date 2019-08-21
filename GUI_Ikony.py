03579c4f7b107e96f0cdfe6511a096664071cf21
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2019-08-21 20:41:14 +0100

    Add files via upload

--- GUI_Ikony.py
+++ GUI_Ikony.py
@@ -1,7 +1,16 @@
 import tkinter
+from PIL import Image, ImageTk
 
 root = tkinter.Tk()
 root.geometry('480x480')
 
+zdjecie = Image.open('znany_model.jpg')
+zdjecie = zdjecie.resize((150,100),Image.ANTIALIAS)
+ikonka = ImageTk.PhotoImage(zdjecie)
+tkinter.Label(root,image=ikonka).pack()
+
+canvas = tkinter.Canvas(root,width=300,height=300)
+canvas.pack()
+canvas.create_image(0,0,anchor=tkinter.NW,image=ikonka)
 
 root.mainloop()
