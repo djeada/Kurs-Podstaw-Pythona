db33e85691a0ffe9e40a5cb4fd6fd988a113521c
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2019-08-25 14:44:27 +0200

    Add files via upload

---
+++ Calki.py
@@ -0,0 +1,17 @@
+import sympy
+import scipy.integrate
+import numpy
+import matplotlib.pyplot as pyplot
+
+x = sympy.Symbol('x')
+print(sympy.integrate(pow(x,2)-3*pow(x,3)+x-25,x))
+
+def funkcja(x):
+    return pow(x,2)-3*pow(x,3)+x-25
+
+wynik = scipy.integrate.quad(funkcja,0,1)
+print(wynik[0])
+
+z = numpy.linspace(-1,3)
+pyplot.plot(z,funkcja(z))
+pyplot.show()
