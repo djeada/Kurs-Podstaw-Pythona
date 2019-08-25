c8df9121484f602e02883b9523b1772b63bb6ce8
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2019-08-25 17:06:28 +0200

    Add files via upload

--- Spolgloski.py
+++ Spolgloski.py
@@ -1,17 +1,29 @@
-spolgloski = "aeiou"
+'''
+Liczymy spolgloski w danym wyrazie
+sprawdzamy czy znak jest litera i jednoczesnie nie jest samogloska
+jesli tak zwiekszamy licznik o 1
+'''
 
-def zlicz_spolgoski_iter(napis):
+samogloski = 'aeiouy'
+
+def zlicz_spolgloski_iter(napis):
     wynik = 0
     for x in napis:
-        if x.isalpha() and x.lower() not in spolgloski:
+        if x.isalpha() and x.lower() not in samogloski:
             wynik += 1
     return wynik
 
-def zlicz_spolgoski_iter(napis):
+def zlicz_spolgloski_rek(napis):
     if len(napis) == 0:
         return 0
-    
-    if napis[0].isalpha() and napis[0].lower() not in spolgloski:
-        return 1 + zlicz_spolgoski_iter(napis[1:])
-    else:
-        return zlicz_spolgoski_iter(napis[1:])
+    if napis[0].isalpha() and napis[0].lower() not in samogloski:
+        return 1 + zlicz_spolgloski_rek(napis[1:])
+    return zlicz_spolgloski_rek(napis[1:])
+
+lista = ['tunczyk', 'Brokuly', 'James', 'Kapitan']
+
+for x in lista:
+    print(zlicz_spolgloski_iter(x))
+    print(zlicz_spolgloski_rek(x))
+
+
