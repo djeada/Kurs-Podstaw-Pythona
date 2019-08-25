a755ca2102872cb15357aec55662170cba3f570d
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2019-08-25 16:37:58 +0200

    Add files via upload

--- DuzeLitery.py
+++ DuzeLitery.py
@@ -1,20 +1,25 @@
-def znajdz_duza_litere_iter(napis):
+'''
+Znajdujemy pierwsze wystapienie duzej litery w wyrazie
+Sukces: zwracamy duza litere
+Porażka: zwracamy -1
+'''
+
+def znajdz_duza_liter_iter(napis):
     for x in napis:
         if x.isupper():
             return x
     return -1
 
-def znajdz_duza_litere_rek(napis):
+def znajdz_duza_liter_rek(napis):
     if len(napis) == 0:
         return -1
     if napis[0].isupper():
         return napis[0]
     else:
-        return znajdz_duza_litere_rek(napis[1:])
+        return znajdz_duza_liter_rek(napis[1:])
 
-lista = ['marcepAn', 'tuNczyk', 'rower']
+lista = ['Marcepan', 'tunCzyk', 'grzegoRz', 'napis']
 
 for x in lista:
-    print(znajdz_duza_litere_iter(x))
-    print(znajdz_duza_litere_rek(x))
-
+    print(znajdz_duza_liter_iter(x))
+    print(znajdz_duza_liter_rek(x))
