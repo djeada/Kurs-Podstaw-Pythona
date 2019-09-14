8fd3e2f29f53dfbc420863839ef8424eb3082d07
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2019-09-14 15:00:33 +0200

    Add files via upload

--- BinarneDrzewoPoszukiwan.py
+++ BinarneDrzewoPoszukiwan.py
@@ -1,65 +1,80 @@
-import random
-
 class Wezel():
-    def __init__(self,dane=None):
+    def __init__(self, dane=None):
         self.dane = dane
         self.lewe_dziecko = None
         self.prawe_dziecko = None
-
-class Binarne_Drzewo_Poszukiwan():
+        
+class BST():
     def __init__(self):
         self.korzen = Wezel()
 
-    def dodaj(self,dane):
+    def dodaj(self, dane):
         if self.korzen.dane == None:
             self.korzen.dane = dane
         else:
-            def dodaj_do_korzenia(dane, korzen):
-                if  dane < korzen.dane:
-                    if korzen.lewe_dziecko == None:
-                        korzen.lewe_dziecko = Wezel(dane)
+            def dodaj_do_wierzcholka(dane, wierzcholek):
+                if dane < wierzcholek.dane:
+                    if wierzcholek.lewe_dziecko == None:
+                        wierzcholek.lewe_dziecko = Wezel(dane)
                     else:
-                        dodaj_do_korzenia(dane, korzen.lewe_dziecko)
-                elif dane > korzen.dane:
-                    if korzen.prawe_dziecko == None:
-                        korzen.prawe_dziecko = Wezel(dane)
+                        dodaj_do_wierzcholka(dane, wierzcholek.lewe_dziecko)
+                if dane > wierzcholek.dane:
+                    if wierzcholek.prawe_dziecko == None:
+                        wierzcholek.prawe_dziecko = Wezel(dane)
                     else:
-                        dodaj_do_korzenia(dane, korzen.prawe_dziecko)
-            dodaj_do_korzenia(dane, self.korzen)
+                        dodaj_do_wierzcholka(dane, wierzcholek.prawe_dziecko)
+            dodaj_do_wierzcholka(dane, self.korzen)
 
     def wyswietl(self):
         wynik = ''
-        def przechodzenie_wzdluzne(wezel, wynik):
-        #Korzeń -> Lewy -> Prawy
-            if wezel:
-                if wezel.dane:
-                    wynik += (str(wezel.dane) + '-')
-                    wynik = przechodzenie_wzdluzne(wezel.lewe_dziecko, wynik)
-                    wynik = przechodzenie_wzdluzne(wezel.prawe_dziecko, wynik)
+        def przechodzenie_wzdluzne(wynik, wierzcholek):
+            #Korzen -> Lewy -> Prawy
+            if wierzcholek:
+                if wierzcholek.dane:
+                    wynik += (str(wierzcholek.dane) + '-')
+                    wynik = przechodzenie_wzdluzne(wynik, wierzcholek.lewe_dziecko)
+                    wynik = przechodzenie_wzdluzne(wynik, wierzcholek.prawe_dziecko)
             return wynik
-        def przechodzenie_poprzeczne(wezel, wynik):
-        #Lewy -> Korzeń -> Prawy
-            if wezel:
-                if wezel.dane:
-                    wynik = przechodzenie_wzdluzne(wezel.lewe_dziecko, wynik)
-                    wynik += (str(wezel.dane) + '-')
-                    wynik = przechodzenie_wzdluzne(wezel.prawe_dziecko, wynik)
+        def przechodzenie_poprzeczne(wynik, wierzcholek):
+            # Lewy -> Korzen -> Prawy
+            if wierzcholek:
+                if wierzcholek.dane:
+                    wynik = przechodzenie_poprzeczne(wynik, wierzcholek.lewe_dziecko)
+                    wynik += (str(wierzcholek.dane) + '-')
+                    wynik = przechodzenie_poprzeczne(wynik, wierzcholek.prawe_dziecko)
             return wynik
-        def przechodzenie_wsteczne(wezel, wynik):
-        #Lewy -> Prawy -> Korzeń
-            if wezel:
-                if wezel.dane:
-                    wynik = przechodzenie_wzdluzne(wezel.lewe_dziecko, wynik)
-                    wynik = przechodzenie_wzdluzne(wezel.prawe_dziecko, wynik)
-                    wynik += (str(wezel.dane) + '-')
+        def przechodzenie_wsteczne(wynik, wierzcholek):
+            # Lewy -> Prawy -> Korzen
+            if wierzcholek:
+                if wierzcholek.dane:
+                    wynik = przechodzenie_wsteczne(wynik, wierzcholek.lewe_dziecko)
+                    wynik = przechodzenie_wsteczne(wynik, wierzcholek.prawe_dziecko)
+                    wynik += (str(wierzcholek.dane) + '-')
             return wynik
-        print(przechodzenie_wzdluzne(self.korzen, wynik))
-        print(przechodzenie_poprzeczne(self.korzen, wynik))
-        print(przechodzenie_wsteczne(self.korzen, wynik))
+        print(przechodzenie_wzdluzne(wynik, self.korzen))
+        print(przechodzenie_poprzeczne(wynik, self.korzen))
+        print(przechodzenie_wsteczne(wynik, self.korzen))
 
-drzewo = Binarne_Drzewo_Poszukiwan()
-for i in range(30):
-    drzewo.dodaj(random.randint(0,50))
 
+drzewo = BST()
+drzewo.dodaj(3)
+drzewo.dodaj(2)
+drzewo.dodaj(1)
+drzewo.dodaj(7)
+drzewo.dodaj(8)
+drzewo.dodaj(99)
 drzewo.wyswietl()
 
+
+
+
+        
+                
+    
+
+
+
+
+        
+                
+    
