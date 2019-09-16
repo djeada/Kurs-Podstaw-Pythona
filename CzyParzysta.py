20caea4fbe1d6730822d20246b34df4d248ac051
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2019-09-16 20:34:40 +0200

    Add files via upload

---
+++ CzyParzysta.py
@@ -0,0 +1,7 @@
+def zbadajParzystosc(liczba):
+    if liczba & 1 == 1:
+        return 'Liczba {} jest nieparzysta '.format(liczba)
+    return 'Liczba {} jest parzysta '.format(liczba)
+
+for x in range(1, 11):
+    print(zbadajParzystosc(x))
