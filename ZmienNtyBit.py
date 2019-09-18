7fa8e07f8361c7d584b460ce387cc668391f0a0f
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2019-09-18 11:57:04 +0200

    Add files via upload

---
+++ ZmienNtyBit.py
@@ -0,0 +1,10 @@
+def zmienNtyBit(liczba, n, bit):
+    if bit == 1:
+        return liczba | (1 << (n-1))
+    elif bit == 0:
+        return liczba & ~(1 << (n-1))
+
+
+print(zmienNtyBit(5,2,1))
+print(zmienNtyBit(5,1,0))
+
