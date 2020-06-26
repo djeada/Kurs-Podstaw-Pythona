6d483f10b8983ab1a2e34930d3e7fc230b83ecd2
djeada <addjellouli1@gmail.com>
2020-06-26 22:48:46 +0200

    porzadki

---
+++ System-binarny/SprawdzNtyBit.py
@@ -0,0 +1,9 @@
+def sprawdzNtyBit(liczba, n):
+    if liczba & (1 << (n-1)):
+        return 1
+    return 0
+
+print(sprawdzNtyBit(17, 1))
+print(sprawdzNtyBit(28, 3))
+print(sprawdzNtyBit(9, 2))
+
