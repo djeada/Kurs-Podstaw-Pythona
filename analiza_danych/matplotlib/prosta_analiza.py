dfb6e85998a92176ce3ea96f7eaddd17974bc54d
djeada <addjellouli1@gmail.com>
2022-02-02 13:28:55 +0100

    zmiana nazw plikow z PascalCase na snake_case

---
+++ analiza_danych/matplotlib/prosta_analiza.py
@@ -0,0 +1,18 @@
+import numpy
+from matplotlib import pyplot
+
+data_file = numpy.loadtxt("data_file.txt", delimiter=",")
+time = data_file[:, 0]
+print(time[:10])
+
+
+sensors = data_file[:, 1:5]
+avg = numpy.mean(sensors, axis=1)
+
+my_data = numpy.vstack((time, sensors.T, avg))
+my_data = my_data.T
+numpy.savetxt("export.csv", my_data, delimiter=",")
+
+pyplot.plot(time / 60, sensors[:, 1], "ro")
+pyplot.plot(time / 60, avg, "b.")
+pyplot.show()
---
+++ zastosowania_biurowe/doc_na_pdf.py
@@ -0,0 +1,26 @@
+import os
+import sys
+import comtypes.client
+
+
+def get_files(dirname):
+    all_files = []
+    for root, dirs, files in os.walk(dirname):
+        for filename in files:
+            if filename[-4:] == "docx":
+                all_files.append(os.path.join(root, filename))
+    return all_files
+
+
+def get_out_name(input_name):
+    return input_name[: len(input_name) - 4] + "pdf"
+
+
+files = get_files(os.getcwd())
+
+for file in files:
+    word = comtypes.client.CreateObject("Word.Application")
+    doc = word.Documents.Open(file)
+    doc.SaveAs(get_out_name(file), FileFormat=17)
+    doc.Close()
+    word.Quit()
