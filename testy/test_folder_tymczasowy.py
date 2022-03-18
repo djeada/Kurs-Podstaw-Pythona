1379b10cba3c1262dff1b50e26a06038c9106ed0
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2022-03-18 23:35:43 +0100

    [dodano przyklad]: test folder tymczasowy

---
+++ testy/test_folder_tymczasowy.py
@@ -0,0 +1,26 @@
+import shutil
+from pathlib import Path
+import unittest
+
+
+class PrzykladTestu(unittest.TestCase):
+    def setUp(self):
+        #
+        self.folder_testowy = Path("temp_dir")
+        self.folder_testowy.mkdir(parents=True, exist_ok=True)
+
+    def tearDown(self):
+        # usun folder testowy z cala zawartoscia
+        shutil.rmtree(self.folder_testowy)
+
+    def przykladowy_test(self):
+        plik = Path("plik.txt")
+        plik.touch()
+        tresc = "Lezy Jerzy na wiezy.\n"
+        plik.write_text(tresc)
+        # sprawdz czy wczytana tresc z pliku pokrywa sie z oryginalna trescia
+        self.assertEqual(plik.read_text(), tresc)
+
+
+if __name__ == "__main__":
+    unittest.main
