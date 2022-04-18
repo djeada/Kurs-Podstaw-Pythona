258293d02a0fe9d77972e2745c82dd4621f9c1d3
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2022-04-18 22:37:52 +0200

    [refaktoryzacja]: napraw ostrzezenie z flake8

--- zastosowania/bazy_danych/sqlite/wyswietl_wiersze.py
+++ zastosowania/bazy_danych/sqlite/wyswietl_wiersze.py
@@ -1,11 +1,13 @@
 import sqlite3
-conn = sqlite3.connect('example.db')
-c = conn.cursor()
+
+polaczenie = sqlite3.connect('example.db')
+kursor = polaczenie.cursor()
 
 nazwa_tabeli = 'pracownicy'
 szukane_pole = 'miejsce_urodzenia'
 szukana_wartosc = 'Warszawa'
 
-c.execute(f"SELECT * from {nazwa_tabeli} where {szukane_pole}={szukana_wartosc}")
-for row in c:
-  print row
+kursor.execute(f"SELECT * from {nazwa_tabeli} where {szukane_pole}={szukana_wartosc}")
+
+for wiersz in kursor:
+  print(wiersz)
